7b9be8d895b7a67322a3c01bf05de5582ab04f7a
Ilias Koritsas <il_koritsas@hotmail.co.uk>
2016-10-05 15:59:56 +0300

    Phys

---
+++ src/main/java/edu/koritsas/slimemold/AbstractPhysarumPolycephalum.java
@@ -0,0 +1,18 @@
+package edu.koritsas.slimemold;
+
+import org.geotools.graph.structure.Graph;
+import org.geotools.graph.structure.Node;
+
+import java.util.List;
+
+/**
+ * Created by ilias on 5/10/2016.
+ */
+public abstract class AbstractPhysarumPolycephalum {
+    private Graph graph;
+    private Node sourceNode;
+    private Node sinkNode;
+    private List<Node> sinkNodesList;
+
+
+}
